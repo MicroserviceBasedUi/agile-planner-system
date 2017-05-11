86d889526f142fc4ac51ad69ce19ae47e3fdd84c
Michael Lehmann <lehmamic@bluewin.ch>
2017-05-11 19:48:03 +0200

    prepare planner settings

--- src/Zuehlke.AgilePlaner.Client/src/components/planning-settings/planning-settings.ts
+++ src/Zuehlke.AgilePlaner.Client/src/components/planning-settings/planning-settings.ts
@@ -2,18 +2,20 @@ import { Sprint, Release, Story, ReleaseScope } from '../shared';
 import { bindable, inject } from "aurelia-framework";
 import { HttpClient, json } from 'aurelia-fetch-client';
 import { EventAggregator } from 'aurelia-event-aggregator';
+import * as moment from 'moment';
 
 @inject(HttpClient, EventAggregator)
 export class PlanningSettings {
 
     public availableSprints: Array<Sprint> = [];
     private endSprintId: string;
+    private startDate: Date = new Date(2017, 0, 1);
+    private sprintLength: number = 2;
 
     constructor(private http: HttpClient, private hub: EventAggregator) {
 
         const self = this;
-        this.http.fetch('http://localhost:8000/api/backlog/sprints')
-            .then(response => <Promise<Array<Sprint>>>response.json())
+        this.loadSprints()
             .then(sprints => {
                 self.availableSprints = sprints;
                 self.selectedEndSprint = sprints[0].name;
@@ -26,8 +28,31 @@ export class PlanningSettings {
         this.publishScope();
     }
 
+    set sprintLengthNumber(value) {
+        this.sprintLength = value;
+    }
+
+    get sprintLengthNumber () {
+        return this.sprintLength;
+    }
+
     get selectedEndSprint() {
         return this.endSprintId;
+    }
+
+    set startDateString(value) {
+        if (value != undefined) {
+            this.startDate = moment(value, 'YYYY-MM-DD').toDate();
+        }
+    }
+
+    get startDateString() {
+        return moment(this.startDate).format('YYYY-MM-DD');
+    }
+
+    private loadSprints() : Promise<Array<Sprint>> {
+        return this.http.fetch('http://localhost:8000/api/backlog/sprints')
+            .then(response => <Promise<Array<Sprint>>>response.json())
     }
 
     private publishScope(): void {
